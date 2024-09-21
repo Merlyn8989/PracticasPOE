1ef9956bd37b5ce279f3580ffacedcf65e00e2bd
Merlyn8989 <merlynrajo@gmail.com>
2024-09-20 21:13:54 -0600

    unir rama

--- CommonLayer/obj/Debug/net8.0/CommonLayer.AssemblyInfo.cs
+++ CommonLayer/obj/Debug/net8.0/CommonLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+434d6a8abeaacf0ef4525157854cf0bd067c1364")]
 [assembly: System.Reflection.AssemblyProductAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CommonLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
